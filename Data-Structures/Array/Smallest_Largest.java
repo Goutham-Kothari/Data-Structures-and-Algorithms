1c3115b96b121f45e0aaa73894a567412775389d
Goutham N Kothari <gouthamnkothari24@gmail.com>
2025-10-18 11:33:39 +0530

    Add Smallest_Largest class to find min and max

---
+++ Data-Structures/Array/Smallest_Largest.java
@@ -0,0 +1,30 @@
+public class Smallest_Largest {
+
+    // /Largest number in array
+    public static void main(String[] args) {
+        int numbers[] = {1, 2, 6, 3, 5};
+        System.out.println("Largest value in array is : "+ getLargest(numbers));
+        System.out.println("Smallest value in array is : "+ getSmallest(numbers));
+
+    }    
+
+    public static int getLargest(int numbers[]) {
+        int largest = Integer.MIN_VALUE; // -infinity
+        for (int i = 0; i < numbers.length; i++) {
+            if (largest < numbers[i]) {
+                largest = numbers[i];
+            }
+        }
+        return largest;
+    }
+
+    public static int getSmallest(int numbers[]) {
+        int smallest = Integer.MAX_VALUE; // +infinity
+        for (int i = 0; i < numbers.length; i++) {
+            if (smallest > numbers[i]) {
+                smallest = numbers[i];
+            } 
+        }
+        return smallest;
+    }
+}
