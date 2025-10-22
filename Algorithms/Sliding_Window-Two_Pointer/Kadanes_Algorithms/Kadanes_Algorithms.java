c249520b2ac7d6db46803b470a3500e2aca39d67
Goutham N Kothari <gouthamnkothari24@gmail.com>
2025-10-22 12:26:11 +0530

    Implement Kadane's Algorithm for max subarray sum

---
+++ Algorithms/Sliding_Window-Two_Pointer/Kadanes_Algorithms/Kadanes_Algorithms.java
@@ -0,0 +1,21 @@
+public class Kadanes_Algorithms {
+
+    public static void main(String[] args) {
+        int nums[] = {-2,-3,4,-1,-2,1,5,-3};
+        kadanes(nums);
+    }
+
+    public static void kadanes(int nums[]) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            currentSum = currentSum + nums[i];
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        System.out.println("Max subarray sum = " + maxSum);
+    }
+}
