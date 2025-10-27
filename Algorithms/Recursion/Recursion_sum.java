b54280edaef2fbcbd1df1b3cefce4461cf336a94
Goutham N Kothari <gouthamnkothari24@gmail.com>
2025-10-27 18:13:01 +0530

    Update Recursion_sum.java

--- Algorithms/Recursion/Recursion_sum.java
+++ Algorithms/Recursion/Recursion_sum.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Recursion_sum {
     public static int nSum(int n) {
         if (n == 1) {
-            return 1;
+            return n;
         }
         int sum = n + nSum(n-1);
         return sum;
