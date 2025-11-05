da2c639fc1657378ccb61facdf853f162eb9d3a2
Goutham N Kothari <gouthamnkothari24@gmail.com>
2025-11-05 15:42:35 +0530

    Implement permutation generation algorithm

---
+++ Algorithms/Backtracking/Permutation.java
@@ -0,0 +1,19 @@
+public class Permutation {
+    public static void findPermutation(String str, String ans) {
+        // Basecase
+        if (str.length() == 0) {
+            System.err.println(ans);
+            return;
+        }
+        // Recursion
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i+1);
+            findPermutation(newStr, ans+curr);
+        }
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        findPermutation(str, "");
+    }
+}
