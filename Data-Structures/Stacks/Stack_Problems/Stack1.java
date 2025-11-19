4f318ac149fbf6edf1b94209d4c7785aabaa2b96
Goutham N Kothari <gouthamnkothari24@gmail.com>
2025-11-19 16:13:48 +0530

    Implement pushAtBottom method in Stack1 class

---
+++ Data-Structures/Stacks/Stack_Problems/Stack1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Stack1 {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        pushAtBottom(s, 4);
+        System.out.println(s);
+    }
+
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+}
